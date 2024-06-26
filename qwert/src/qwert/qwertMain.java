f60475ae5a8d0eeb6bc3547b5d10ee56ed9f5c9d
topnbiz <topnbiz@gmail.com>
2024-06-26 16:31:10 +0900

    졸리니까 빨리 올리고 집에가자~

---
+++ qwert/src/qwert/qwertMain.java
@@ -0,0 +1,14 @@
+package qwert;
+
+public class qwertMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("이클립스");
+		System.out.println("로컬 저장소");
+		System.out.println("원격 저장소");
+	}
+
+}
